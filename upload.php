2e150f63b4a5cdabf0d570a8ca523c58bb768f5c
jaxeuseuse <62519609+jaxeuseuse@users.noreply.github.com>
2024-03-29 09:43:12 -0400

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,23 @@
+<?php
+$targetDir = "/var/www/taken/";
+
+if (!empty($_FILES["file"])) {
+    $fileName = basename($_FILES["file"]["name"]);
+    $targetFilePath = $targetDir . $fileName;
+    $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+
+    // Check if file already exists
+    if (file_exists($targetFilePath)) {
+        echo "Sorry, file already exists.";
+    } else {
+        // Upload file to server
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+            echo "The file ".$fileName. " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+} else {
+    echo "No file uploaded.";
+}
+?>
