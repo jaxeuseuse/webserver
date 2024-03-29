e64c7c3c5688b16e49d97c2cda9d28b2927ea008
jaxeuseuse <62519609+jaxeuseuse@users.noreply.github.com>
2024-03-29 16:27:28 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,21 +1,19 @@
 <?php
 $targetDir = "/var/www/taken/";
-chmod("/var/www/taken/",0777);
+chmod("/var/www/taken/", 0777);
 if (!empty($_FILES["file"])) {
-    $fileName = basename($_FILES["file"]["name"]);
-    $targetFilePath = $targetDir . $fileName;
-    $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+    $fileName = basename($_FILES["file"]["name"]); // Vulnerability 2
+    $targetFilePath = $targetDir . $fileName; // Vulnerability 3
+    //$fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); // Vulnerability 1
 
     // Check if file already exists
-    if (file_exists($targetFilePath)) {
-        echo "Sorry, file already exists.";
+    // Removed the check for existing files
+
+    // Upload file to server
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+        echo "The file " . $fileName . " has been uploaded.";
     } else {
-        // Upload file to server
-        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
-            echo "The file ".$fileName. " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
+        echo "Sorry, there was an error uploading your file.";
     }
 } else {
     echo "No file uploaded.";
